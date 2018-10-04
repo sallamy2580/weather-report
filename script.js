5544819154eb28425d7c9a3ff0d676b112a40a43
Superpol@ris <sallamy2580@gmail.com>
2018-10-04 16:29:34 -0600

    made the dearch button clickable with the data showing in the console log. When i click the button, the data is populated ion concole and in the next dashboard window, in the h2

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
+    
+    function getCity (){
+        $(".submitCity").click(function(){
+            var cityData = $(".searchBar").val();
+            var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityData + "&appid=c64ae898096dd7b2fab2998d1d228df8"
+            
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+            console.log(response);
+            showWeather();
+        });
+        })
+    }
+    
+    getCity();
 
+    function showWeather(){
+        var cityData = $(".searchBar").val();
+        $(".currentCity").text(cityData);
+        //capitalize first letter
+    }
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=denver&appid=c64ae898096dd7b2fab2998d1d228df8"
-
-
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    });
-
+   
 });
 
